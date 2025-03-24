1614128e23d74f896028b1ad0f7b8360af5af445
chanwoo040531 <chanwoo040531@gmail.com>
2025-03-24 10:05:11 +0900

    initialize

---
+++ src/main/java/com/practice/paymentassignment/PaymentAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.practice.paymentassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentAssignmentApplication.class, args);
+    }
+
+}
